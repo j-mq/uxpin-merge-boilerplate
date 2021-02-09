f5b3978f0b30863ac3b6dedaffa48933ba5d276f
Jacobo <jacobo@fixel.co.jp>
2021-02-09 16:19:12 +0900

    updating circleci config

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Icon from "../../Icon/Icon";
-import Button from "../Button";
+import React from 'react';
+import Icon from '../../Icon/Icon';
+import Button from '../Button';
 
 export default (
   <Button
