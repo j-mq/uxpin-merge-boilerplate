9f881eba061b3ffaa136979693ba9958c57aa153
Jacobo <jacobo@fixel.co.jp>
2021-02-10 17:10:28 +0900

    corrections

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import "../../styles/FDSColors/colors.scss";
-
 export default function UXPinWrapper({ children }) {
   //Appending font
   //This solution will likely be discontinued in the future. Merge prefers direct imports of all the files handled with Webpack.
