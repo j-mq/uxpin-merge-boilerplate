62926216860be96423bca520a3f54a3f2a3bdffb
Jacobo <jacobo@fixel.co.jp>
2021-02-10 17:14:06 +0900

    typo

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -4,11 +4,11 @@ export default function UXPinWrapper({ children }) {
   //Appending font
   //This solution will likely be discontinued in the future. Merge prefers direct imports of all the files handled with Webpack.
   let notoSans = document.createElement("link");
-  roboto.setAttribute(
+  notoSans.setAttribute(
     "href",
     "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
   );
-  roboto.setAttribute("rel", "stylesheet");
+  notoSans.setAttribute("rel", "stylesheet");
   document.head.appendChild(notoSans);
 
   return children;
